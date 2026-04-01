7856dffeeb625641525dc82448c0607cb0eabd4e
Felipe Bezerra <nas.felipe73@gmail.com>
2026-04-01 19:32:32 -0300

    Refactor open_dex and enhance main interface
    Updated the open_dex function to create a new window with a specific title and background color. Enhanced the main interface with improved styling and labels.

--- nvdex2.0.py
+++ nvdex2.0.py
@@ -4,10 +4,27 @@ from tkinter import ttk
 ##FUNÇÕES
 def open_dex():
 
+    main_dex.destroy()#abre uma janela 'tk' vazia
     nat_dex = tk.Toplevel()
     nat_dex.title("NATIONAL DEX")
+    nat_dex.geometry("500x700+700+180")
+
+    nat_dex.config(bg='#790d0d')
 
     
+    approach_nat_dex = ttk.Label(
+        nat_dex,
+        text="Which Creature do you want to Research?",
+        font=("Helvetica",15),
+        foreground="#b3a125",
+        anchor="center",
+        background='#790d0d'
+        )
+    nat_dex.pack(ipadx=60, ipady=40)
+
+    entry_nat_dex = ttk.Entry
+    #trazer entries
+
 
 
 ##INTERFACE
@@ -16,12 +33,15 @@ main_dex = tk.Tk()
 main_dex.title("NVDEX")
     #proporções
 main_dex.geometry("500x700+700+180")
+main_dex.config(bg='#790d0d')
 
 titulo = ttk.Label(
     main_dex,
     text="nvDEX",
-    font=("Poke",15),
+    font=("Helvetica",15),
+    foreground="#b3a125",
     anchor="center",
+    background='#790d0d'
     )
 titulo.pack(ipadx=60, ipady=40)
 
@@ -29,8 +49,10 @@ texto_main_dex = ttk.Label(
     main_dex,
     text = "HELLO TRAINER, WELCOME TO THE NATIONAL POKÉDEX",
     font =("Helvetica", 10),
+    foreground="#b3a125",
     anchor="center",
     justify="center",
+    background='#790d0d' 
     )
 texto_main_dex.pack(ipadx=20, ipady=10)
 
